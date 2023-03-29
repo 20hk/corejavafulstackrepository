982f4ceaa336af5f593a8766a40624e59e299749
harshithkumar <harshithkumar820@gmail.com>
2023-03-29 13:23:05 +0530

    operator example added

---
+++ src/harshithk/operators.java
@@ -0,0 +1,16 @@
+package harshithk;
+
+public class operators {
+	public static void main (String[args]) {
+		int a= 10,b=20;
+		int result=(a<b)?a:b;
+		System.out.printLn(result);
+		System.out.printLn(a+b);
+		System.out.printLn(a-b);
+		System.out.printLn(a*b);
+		System.out.printLn(a/b);
+		System.out.printLn(b%a);
+		
+	}
+
+}
